04a8dc416df3ccfa6a2e6f7dd3e577d3940d0a4b
Daichi <daichi.ninomiya@deer.co.jp>
2019-02-06 02:35:24 +0900

    fix

--- slash.js
+++ slash.js
@@ -1,29 +1,31 @@
-const express = require('express')
+const app = require('express')()
 const bodyParser = require('body-parser')
-const app = express()
+const multer = require('multer')
+const upload = multer()
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
-app.get('*', (req, res) => {
+app.get('*', upload.array(), (req, res) => {
   const text = req.body && req.body.text
-  let msg;
+  let msg
 
   switch (text) {
     case 'ggr':
       msg = `Googleで調べれば解決できそうです！ :google: いい機会なので調べてみてください！`
-      break;
+      break
     case 'doc':
       msg = `
         ドキュメントで解説されています。検索してみましょう
         https://docs.update.jp
-      `;
-      break;
+      `
+      break
     case 'help':
       msg = `
       doc: ドキュメントで調べましょう
       ggr: Googleで調べましょう
       `
+      break
     default:
       msg = `コマンドが見つかりません`
   }
