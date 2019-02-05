9af9ddddc396ac94512757f15362781224433e1a
Daichi <daichi.ninomiya@deer.co.jp>
2019-02-06 01:44:07 +0900

    update

--- slash.js
+++ slash.js
@@ -1,25 +1,27 @@
-module.exports = (req, res) => {
-  console.log(req);
-  // const { text } = req.body;
+const { parse } = require('url')
 
-  // let msg;
-  // switch (text) {
-  //   case 'ggr':
-  //     msg = `Googleで調べれば解決できそうです！ :google: いい機会なので調べてみてください！`;
-  //     break;
-  //   case 'doc':
-  //     msg = `
-  //       ドキュメントで解説されています。検索してみましょう
-  //       https://docs.update.jp
-  //     `;
-  //     break;
-  //   case 'help':
-  //     msg = `
-  //     doc: ドキュメントで調べましょう
-  //     ggr: Googleで調べましょう
-  //     `
-  //   default:
-  //     msg = `コマンドが見つかりません`;
-  // }
+module.exports = (req, res) => {
+  const { query } = parse(req.url, true)
+  const { text } = query;
+
+  let msg;
+  switch (text) {
+    case 'ggr':
+      msg = `Googleで調べれば解決できそうです！ :google: いい機会なので調べてみてください！`;
+      break;
+    case 'doc':
+      msg = `
+        ドキュメントで解説されています。検索してみましょう
+        https://docs.update.jp
+      `;
+      break;
+    case 'help':
+      msg = `
+      doc: ドキュメントで調べましょう
+      ggr: Googleで調べましょう
+      `
+    default:
+      msg = `コマンドが見つかりません ${query} ${JSON.stringify(query)}`;
+  }
   res.end(msg);
 }
