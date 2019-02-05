66cbd66c115cbcf0cdc4a44776a76e7b3233db9b
Daichi <daichi.ninomiya@deer.co.jp>
2019-02-06 01:17:56 +0900

    update command

--- slash.js
+++ slash.js
@@ -2,6 +2,25 @@ const { parse } = require('url')
 
 module.exports = (req, res) => {
   const { query } = parse(req.url, true)
-  const { name = 'World' } = query
-  res.end(`Hello ${name}!`)
+  const { text } = query
+  let msg;
+  switch (text) {
+    case 'ggr':
+      msg = `Googleで調べれば解決できそうです！ :google: いい機会なので調べてみてください！`;
+      break;
+    case 'doc':
+      msg = `
+        ドキュメントで解説されています。検索してみましょう
+        https://docs.update.jp
+      `;
+      break;
+    case '':
+      msg = `
+      doc: ドキュメントで調べましょう
+      ggr: Googleで調べましょう
+      `
+    default:
+      msg = 'コマンドが見つかりません';
+  }
+  res.end(msg)
 }
