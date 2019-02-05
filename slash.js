3e839de88a2775b51c2ed322bb5a2ef4f2a3ca1d
Daichi <daichi.ninomiya@deer.co.jp>
2019-02-06 02:24:29 +0900

    fix

--- slash.js
+++ slash.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('*', (req, res) => {
-  const text = req.body.text;
+  const text = req.body && req.body.text && null;
   let msg;
 
   switch (text) {
