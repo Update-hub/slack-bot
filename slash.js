fce88af0778f3138b605c4ffc9a191d894e29524
Daichi <daichi.ninomiya@deer.co.jp>
2019-02-06 03:11:33 +0900

    update command

--- slash.js
+++ slash.js
@@ -9,19 +9,24 @@ app.post('*', (req, res) => {
   let msg
 
   switch (text) {
+    case 'try':
+      msg = `
+その問題は解決できそうなので、まずは<https://docs.update.jp/projects/flow#karanaitsumadzuita|トラブルシューティング>を見てトライしてみましょう！
+      `
+      break
     case 'ggr':
-      msg = `Googleで調べれば解決できそうです！ :google: いい機会なので調べてみてください！`
+      msg = `:google:Googleで調べれば解決できそうです！いい機会なので調べてみてください！`
       break
     case 'doc':
       msg = `
-        ドキュメントで解説されています。検索してみましょう
-        https://docs.update.jp
+ドキュメントで解説されています。検索してみましょう
+https://docs.update.jp
       `
       break
     case 'help':
       msg = `
-      doc: ドキュメントで調べましょう
-      ggr: Googleで調べましょう
+doc: ドキュメントで調べましょう
+ggr: Googleで調べましょう
       `
       break
     default:
